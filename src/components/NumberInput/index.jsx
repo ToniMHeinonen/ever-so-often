a85aa56c94573499775c402a1089938b755bc665
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-12-22 10:54:55 +0200

    Add NumberInput component
    - Render number value and limit the value to not be lower than minValue.
    - Add increase and decrease buttons on the number field sides.

---
+++ src/components/NumberInput/index.jsx
@@ -0,0 +1,70 @@
+import TextInput from '../../styles/TextInput'
+import IconButton from '../IconButton'
+import {
+  Container,
+  minusButtonStyle,
+  numberInputStyle,
+  plusButtonStyle,
+} from './style'
+
+const NumberInput = ({
+  value,
+  onChange,
+  onBlur,
+  minValue = 1,
+  allowDecimals,
+  ...props
+}) => {
+  const onFocusLost = () => {
+    const textValue = Number(value)
+    const limitedValue = parseNumber(textValue)
+
+    onChange(limitedValue.toString())
+    onBlur()
+  }
+
+  const parseNumber = (value) => {
+    const limited = isNaN(value) ? minValue : Math.max(value, minValue)
+    return allowDecimals ? limited : Math.round(limited)
+  }
+
+  const decrease = () => {
+    const textValue = Number(value)
+    const num = parseNumber(textValue - 1)
+    onChange(num.toString())
+  }
+
+  const increase = () => {
+    const textValue = Number(value)
+    const num = parseNumber(textValue + 1)
+    onChange(num.toString())
+  }
+
+  return (
+    <Container>
+      <IconButton
+        name="remove"
+        size={22}
+        style={minusButtonStyle}
+        onPress={decrease}
+      />
+      <TextInput
+        textAlign="center"
+        keyboardType="numeric"
+        style={numberInputStyle}
+        onChangeText={onChange}
+        onBlur={onFocusLost}
+        value={value}
+        {...props}
+      />
+      <IconButton
+        name="add"
+        size={22}
+        style={plusButtonStyle}
+        onPress={increase}
+      />
+    </Container>
+  )
+}
+
+export default NumberInput
---
+++ src/components/NumberInput/style.js
@@ -0,0 +1,43 @@
+import styled, { css } from 'styled-components/native'
+import { pressedContentColor } from '../../styles/fragments'
+import theme from '../theme'
+
+export const Container = styled.View`
+  flex-direction: row;
+`
+
+export const minusButtonStyle = css`
+  border-right-width: 2px;
+  margin-right: 0px;
+  border-top-right-radius: 0px;
+  border-bottom-right-radius: 0px;
+  border-top-color: ${theme.colors.accent};
+  border-left-color: ${theme.colors.accent};
+  border-bottom-color: ${theme.colors.accent};
+  border-right-color: ${theme.colors.appBackground};
+  background-color: ${theme.colors.appBarBackground};
+
+  ${pressedContentColor}
+`
+
+export const numberInputStyle = css`
+  margin-left: 0px;
+  margin-right: 0px;
+  border-radius: 0px;
+  padding-left: 15px;
+  padding-right: 15px;
+`
+
+export const plusButtonStyle = css`
+  border-left-width: 2px;
+  margin-left: 0px;
+  border-top-left-radius: 0px;
+  border-bottom-left-radius: 0px;
+  border-top-color: ${theme.colors.primary};
+  border-right-color: ${theme.colors.primary};
+  border-bottom-color: ${theme.colors.primary};
+  border-left-color: ${theme.colors.appBackground};
+  background-color: ${theme.colors.appBarBackground};
+
+  ${pressedContentColor}
+`

c2a704a79ce952fa5eeccd80717f7b1c0bee65ff
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-12-23 09:33:41 +0200

    Change status bar content color
    Make content color light, so it is more visible with current color scheme.

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,12 +1,18 @@
+import { StatusBar } from 'expo-status-bar'
 import { Route, Routes, Navigate } from 'react-router-native'
 import AppBar from '../AppBar'
 import NewReminder from '../NewReminder'
 import ThisDayPage from '../ThisDayPage'
 import { Container } from './style'
+import theme from '../theme'
 
 const Main = () => {
   return (
     <Container>
+      <StatusBar
+        backgroundColor={theme.colors.appBarBackground}
+        style="light"
+      />
       <AppBar />
       <Routes>
         <Route path="/" element={<ThisDayPage />} exact />

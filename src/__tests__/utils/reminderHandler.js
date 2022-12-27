35240fdeb5887f34a0dd7a8bb65c5e8bcf1ab4c8
ToniMHeinonen <toni.heinonen@hotmail.fi>
2022-12-27 10:04:55 +0200

    Add multiple functions to reminderHandler utility
    - Add getActiveDay(), sortRemindersByActiveState() and splitRemindersByActiveState().
    - Add documentation comments to reminderHandler functions.
    - Add tests for reminderHandler functions.

--- src/__tests__/utils/reminderHandler.js
+++ src/__tests__/utils/reminderHandler.js
@@ -1,6 +1,9 @@
 import {
+  getActiveDay,
   getReminderActiveDay,
   getReminderMaxDay,
+  sortRemindersByActiveState,
+  splitRemindersByActiveState,
 } from '../../utils/reminderHandler'
 
 describe('getReminderMaxDay()', () => {
@@ -51,5 +54,190 @@ describe('getReminderActiveDay()', () => {
   })
   it('month 2 day 1 should return 2', () => {
     expect(getReminderActiveDay(reminder, new Date('2022/02/01'))).toBe(2)
+  })
+})
+
+describe('getActiveDay()', () => {
+  const reminder = {
+    startDate: '2022/01/01',
+    activities: [
+      { name: 'Activity 1', day: 1 },
+      { name: 'Activity 3', day: 3 },
+      { name: 'Activity 5', day: 5 },
+    ],
+  }
+
+  it('start date should return 1', () => {
+    expect(getActiveDay(reminder, new Date('2022/01/01')).day).toBe(1)
+  })
+
+  it('second day should return undefined', () => {
+    expect(getActiveDay(reminder, new Date('2022/01/02'))).toBe(undefined)
+  })
+  it('third day should return 3', () => {
+    expect(getActiveDay(reminder, new Date('2022/01/03')).day).toBe(3)
+  })
+  it('fourth day should return undefined', () => {
+    expect(getActiveDay(reminder, new Date('2022/01/04'))).toBe(undefined)
+  })
+  it('fifth day should return 5', () => {
+    expect(getActiveDay(reminder, new Date('2022/01/05')).day).toBe(5)
+  })
+  it('sixth day should return 1', () => {
+    expect(getActiveDay(reminder, new Date('2022/01/06')).day).toBe(1)
+  })
+})
+
+describe('Active state handlers', () => {
+  const reminder1 = {
+    value: 1,
+    startDate: '2022/01/01',
+    activities: [
+      { name: 'Activity 3', day: 3 },
+      { name: 'Activity 5', day: 5 },
+    ],
+  }
+
+  const reminder2 = {
+    value: 2,
+    startDate: '2022/01/01',
+    activities: [
+      { name: 'Activity 1', day: 1 },
+      { name: 'Activity 5', day: 5 },
+    ],
+  }
+
+  const reminder3 = {
+    value: 3,
+    startDate: '2022/01/01',
+    activities: [
+      { name: 'Activity 3', day: 3 },
+      { name: 'Activity 5', day: 5 },
+    ],
+  }
+
+  const reminder4 = {
+    value: 4,
+    startDate: '2022/01/01',
+    activities: [
+      { name: 'Activity 1', day: 1 },
+      { name: 'Activity 3', day: 3 },
+      { name: 'Activity 4', day: 4 },
+    ],
+  }
+
+  const reminders = [reminder1, reminder2, reminder3, reminder4]
+
+  describe('sortRemindersByActiveState()', () => {
+    it('start date return order should be 2, 4, 1, 3', () => {
+      const sorted = sortRemindersByActiveState(
+        reminders,
+        new Date('2022/01/01')
+      )
+      expect(sorted[0].value).toBe(2)
+      expect(sorted[1].value).toBe(4)
+      expect(sorted[2].value).toBe(1)
+      expect(sorted[3].value).toBe(3)
+    })
+
+    it('second day return order should be 1, 2, 3, 4', () => {
+      const sorted = sortRemindersByActiveState(
+        reminders,
+        new Date('2022/01/02')
+      )
+      expect(sorted[0].value).toBe(1)
+      expect(sorted[1].value).toBe(2)
+      expect(sorted[2].value).toBe(3)
+      expect(sorted[3].value).toBe(4)
+    })
+
+    it('third day return order should be 1, 3, 4, 2', () => {
+      const sorted = sortRemindersByActiveState(
+        reminders,
+        new Date('2022/01/03')
+      )
+      expect(sorted[0].value).toBe(1)
+      expect(sorted[1].value).toBe(3)
+      expect(sorted[2].value).toBe(4)
+      expect(sorted[3].value).toBe(2)
+    })
+
+    it('fourth day return order should be 4, 1, 2, 3', () => {
+      const sorted = sortRemindersByActiveState(
+        reminders,
+        new Date('2022/01/04')
+      )
+      expect(sorted[0].value).toBe(4)
+      expect(sorted[1].value).toBe(1)
+      expect(sorted[2].value).toBe(2)
+      expect(sorted[3].value).toBe(3)
+    })
+
+    it('fifth day return order should be 1, 2, 3, 4', () => {
+      const sorted = sortRemindersByActiveState(
+        reminders,
+        new Date('2022/01/05')
+      )
+      expect(sorted[0].value).toBe(1)
+      expect(sorted[1].value).toBe(2)
+      expect(sorted[2].value).toBe(3)
+      expect(sorted[3].value).toBe(4)
+    })
+  })
+
+  describe('splitRemindersByActiveState()', () => {
+    it('start date should return [2, 4] and [1, 3]', () => {
+      const [active, inactive] = splitRemindersByActiveState(
+        reminders,
+        new Date('2022/01/01')
+      )
+      expect(active[0].value).toBe(2)
+      expect(active[1].value).toBe(4)
+      expect(inactive[0].value).toBe(1)
+      expect(inactive[1].value).toBe(3)
+    })
+    it('second day should return [] and [1, 2, 3, 4]', () => {
+      const [active, inactive] = splitRemindersByActiveState(
+        reminders,
+        new Date('2022/01/02')
+      )
+      expect(active.length).toBe(0)
+      expect(inactive[0].value).toBe(1)
+      expect(inactive[1].value).toBe(2)
+      expect(inactive[2].value).toBe(3)
+      expect(inactive[3].value).toBe(4)
+    })
+    it('third day should return [1, 3, 4] and [2]', () => {
+      const [active, inactive] = splitRemindersByActiveState(
+        reminders,
+        new Date('2022/01/03')
+      )
+      expect(active[0].value).toBe(1)
+      expect(active[1].value).toBe(3)
+      expect(active[2].value).toBe(4)
+      expect(inactive[0].value).toBe(2)
+    })
+    it('fourth day should return [4] and [1, 2, 3]', () => {
+      const [active, inactive] = splitRemindersByActiveState(
+        reminders,
+        new Date('2022/01/04')
+      )
+      expect(active[0].value).toBe(4)
+      expect(inactive[0].value).toBe(1)
+      expect(inactive[1].value).toBe(2)
+      expect(inactive[2].value).toBe(3)
+    })
+    it('fifth day should return [1, 2, 3, 4] and []', () => {
+      const [active, inactive] = splitRemindersByActiveState(
+        reminders,
+        new Date('2022/01/05')
+      )
+      expect(inactive.length).toBe(0)
+      expect(active[0].value).toBe(1)
+      expect(active[1].value).toBe(2)
+      expect(active[2].value).toBe(3)
+      // 4 is active since it has 4 max days, so Activity 1 is active
+      expect(active[3].value).toBe(4)
+    })
   })
 })
--- src/utils/reminderHandler.js
+++ src/utils/reminderHandler.js
@@ -1,10 +1,21 @@
 import { differenceInDays } from 'date-fns'
 import _ from 'lodash'
 
+/**
+ * Retrieves maximum activity day for reminder.
+ * @param {object} reminder reminder to check
+ * @returns {number} maximum activity day
+ */
 export const getReminderMaxDay = (reminder) => {
   return _.maxBy(reminder.activities, (a) => a.day).day
 }
 
+/**
+ * Retrieves the reminder active day number.
+ * @param {object} reminder reminder to check
+ * @param {Date} currentDate current date
+ * @returns {number} active day number
+ */
 export const getReminderActiveDay = (reminder, currentDate) => {
   const maxDay = getReminderMaxDay(reminder)
   const daysFromStartDate = differenceInDays(
@@ -13,4 +24,44 @@ export const getReminderActiveDay = (reminder, currentDate) => {
   )
 
   return (daysFromStartDate % maxDay) + 1
+}
+
+/**
+ * Retrieves the active day for reminder.
+ * @param {object} reminder reminder to check
+ * @param {Date} currentDate current date
+ * @returns found active activity, undefined if not active
+ */
+export const getActiveDay = (reminder, currentDate) => {
+  const activeDay = getReminderActiveDay(reminder, currentDate)
+  return _.find(reminder.activities, { day: activeDay })
+}
+
+/**
+ * Sorts reminders by their active state.
+ * @param {[object]} reminders reminders to sort
+ * @param {Date} currentDate current date
+ * @returns {[object]} sorted reminders by active state, active reminders first
+ */
+export const sortRemindersByActiveState = (reminders, currentDate) => {
+  const sorted = _.sortBy(
+    reminders,
+    (r) => getActiveDay(r, currentDate) === undefined
+  )
+  return sorted
+}
+
+/**
+ * Splits reminders by their active state.
+ * @param {[object]} reminders reminders to split
+ * @param {Date} currentDate current date
+ * @returns {[[object], [object]]} splitted reminders by active state, active in first array
+ */
+export const splitRemindersByActiveState = (reminders, currentDate) => {
+  const [active, inactive] = _.partition(reminders, (r) => {
+    const day = getActiveDay(r, currentDate)
+    console.log('day', day)
+    return getActiveDay(r, currentDate) !== undefined
+  })
+  return [active, inactive]
 }

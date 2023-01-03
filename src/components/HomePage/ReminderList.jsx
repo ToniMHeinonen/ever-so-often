3a487e63ca60528796957604ea309e929887f407
ToniMHeinonen <toni.heinonen@hotmail.fi>
2023-01-03 09:15:10 +0200

    Hide other activities if empty
    - Hide the title if it is empty.
    - Show "No activities today" text if Today's activities are empty.

--- src/components/HomePage/ReminderList.jsx
+++ src/components/HomePage/ReminderList.jsx
@@ -1,6 +1,7 @@
 import { View } from 'react-native'
 import { useNavigate } from 'react-router-native'
 import SizedBox from '../../styles/SizedBox'
+import Text from '../../styles/Text'
 import Reminder from './Reminder'
 import {
   ReminderHeader,
@@ -18,28 +19,41 @@ const ReminderList = ({ currentDate, activeReminders, inactiveReminders }) => {
   return (
     <ReminderListContainer>
       <ReminderHeader title>{"Today's Activities"}</ReminderHeader>
-      {activeReminders.map((r, index) => (
-        <View key={r.id}>
-          <Reminder
-            reminder={r}
-            currentDate={currentDate}
-            onPress={reminderOnPress}
-          />
-          {index < activeReminders.length - 1 && <ReminderSeparator />}
-        </View>
-      ))}
+      {activeReminders.length > 0 ? (
+        <>
+          {activeReminders.map((r, index) => (
+            <View key={r.id}>
+              <Reminder
+                reminder={r}
+                currentDate={currentDate}
+                onPress={reminderOnPress}
+              />
+              {index < activeReminders.length - 1 && <ReminderSeparator />}
+            </View>
+          ))}
+        </>
+      ) : (
+        <Text title center>
+          No activities today
+        </Text>
+      )}
+
       <SizedBox height={20} />
-      <ReminderHeader title>{'Other Activities'}</ReminderHeader>
-      {inactiveReminders.map((r, index) => (
-        <View key={r.id}>
-          <Reminder
-            reminder={r}
-            currentDate={currentDate}
-            onPress={reminderOnPress}
-          />
-          {index < inactiveReminders.length - 1 && <ReminderSeparator />}
-        </View>
-      ))}
+      {inactiveReminders.length > 0 && (
+        <>
+          <ReminderHeader title>{'Other Activities'}</ReminderHeader>
+          {inactiveReminders.map((r, index) => (
+            <View key={r.id}>
+              <Reminder
+                reminder={r}
+                currentDate={currentDate}
+                onPress={reminderOnPress}
+              />
+              {index < inactiveReminders.length - 1 && <ReminderSeparator />}
+            </View>
+          ))}
+        </>
+      )}
     </ReminderListContainer>
   )
 }

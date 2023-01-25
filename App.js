687c0851a88a69be83e76fb69a60e7ff1d6a29bf
ToniMHeinonen <toni.heinonen@hotmail.fi>
2023-01-25 13:25:16 +0200

    Modify navigation colors
    - Move NavigationContainer from App.js to Main component.
    - Apply theme for the navigation in order to use the app background color on all screen.
    - Wrap NavigationContainer inside of Container in order to remove white flashes during screen animations.

--- App.js
+++ App.js
@@ -5,23 +5,20 @@ import ReminderStorageContext from './src/contexts/ReminderStorageContext'
 import { StateProvider, reducer } from './src/state'
 import LocalStorage from './src/utils/localStorage'
 import LocalStorageContext from './src/contexts/LocalStorageContext'
-import { NavigationContainer } from '@react-navigation/native'
 
 const reminderStorage = new ReminderStorage()
 const localStorage = new LocalStorage()
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <ReminderStorageContext.Provider value={reminderStorage}>
-        <LocalStorageContext.Provider value={localStorage}>
-          <StateProvider reducer={reducer}>
-            <Main />
-            <ModalPortal />
-          </StateProvider>
-        </LocalStorageContext.Provider>
-      </ReminderStorageContext.Provider>
-    </NavigationContainer>
+    <ReminderStorageContext.Provider value={reminderStorage}>
+      <LocalStorageContext.Provider value={localStorage}>
+        <StateProvider reducer={reducer}>
+          <Main />
+          <ModalPortal />
+        </StateProvider>
+      </LocalStorageContext.Provider>
+    </ReminderStorageContext.Provider>
   )
 }
 
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -7,37 +7,48 @@ import AlertDialog from '../AlertDialog'
 import BottomAppBar from '../BottomAppBar'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { useState } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
 
 const Stack = createNativeStackNavigator()
+
+const navigationTheme = {
+  dark: true,
+  colors: {
+    primary: theme.colors.primary,
+    background: theme.colors.appBackground,
+  },
+}
 
 const Main = () => {
   const [state, setState] = useState()
 
   return (
     <Container>
-      <StatusBar
-        backgroundColor={theme.colors.appBarBackground}
-        style="light"
-      />
-      <StatusBarStyle />
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{ headerShown: false }}
-        screenListeners={{
-          state: (e) => {
-            setState(e.data.state)
-          },
-        }}
-      >
-        <Stack.Screen name="Home" component={HomePage} />
-        <Stack.Screen
-          name="Reminder"
-          component={ReminderPage}
-          initialParams={{}}
+      <NavigationContainer theme={navigationTheme}>
+        <StatusBar
+          backgroundColor={theme.colors.appBarBackground}
+          style="light"
         />
-      </Stack.Navigator>
-      <AlertDialog />
-      <BottomAppBar state={state} />
+        <StatusBarStyle />
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{ headerShown: false }}
+          screenListeners={{
+            state: (e) => {
+              setState(e.data.state)
+            },
+          }}
+        >
+          <Stack.Screen name="Home" component={HomePage} />
+          <Stack.Screen
+            name="Reminder"
+            component={ReminderPage}
+            initialParams={{}}
+          />
+        </Stack.Navigator>
+        <AlertDialog />
+        <BottomAppBar state={state} />
+      </NavigationContainer>
     </Container>
   )
 }

b51ce22d9cc4a410812d1cab1f71db26dad7cbbc
ToniMHeinonen <toni.heinonen@hotmail.fi>
2023-01-30 10:40:16 +0200

    Add test for undefined names in reminder form
    Move one test under "new reminder" describe block.

--- src/__tests__/components/ReminderPage.js
+++ src/__tests__/components/ReminderPage.js
@@ -8,38 +8,53 @@ import {
 describe('ReminderPage', () => {
   describe('ReminderForm', () => {
     describe('ReminderFormContainer', () => {
-      it('calls on submit with defined names and initial values', async () => {
+      describe('new reminder', () => {
+        it('calls on submit with defined names and initial values', async () => {
+          const onSubmit = jest.fn()
+          const { getByPlaceholderText, getByText } = render(
+            <ReminderFormContainer onSubmit={onSubmit} />
+          )
+
+          await waitFor(() => {
+            fireEvent.changeText(
+              getByPlaceholderText('Reminder name...'),
+              'Reminder'
+            )
+            fireEvent.changeText(
+              getByPlaceholderText('Activity for the day...'),
+              'Activity'
+            )
+
+            fireEvent.press(getByText('Submit'))
+          })
+
+          expect(onSubmit).toHaveBeenCalledTimes(1)
+
+          // onSubmit.mock.calls[0][0] contains the first argument of the first call
+          expect(onSubmit.mock.calls[0][0]).toEqual({
+            ...initialValues,
+            name: 'Reminder',
+            activities: [
+              {
+                ...initialActivity,
+                name: 'Activity',
+              },
+            ],
+          })
+        })
+      })
+
+      it('throws error if names are not defined', async () => {
         const onSubmit = jest.fn()
-        const { getByPlaceholderText, getByText } = render(
+        const { getByText, getAllByText } = render(
           <ReminderFormContainer onSubmit={onSubmit} />
         )
 
         await waitFor(() => {
-          fireEvent.changeText(
-            getByPlaceholderText('Reminder name...'),
-            'Reminder'
-          )
-          fireEvent.changeText(
-            getByPlaceholderText('Activity for the day...'),
-            'Activity'
-          )
-
           fireEvent.press(getByText('Submit'))
         })
 
-        expect(onSubmit).toHaveBeenCalledTimes(1)
-
-        // onSubmit.mock.calls[0][0] contains the first argument of the first call
-        expect(onSubmit.mock.calls[0][0]).toEqual({
-          ...initialValues,
-          name: 'Reminder',
-          activities: [
-            {
-              ...initialActivity,
-              name: 'Activity',
-            },
-          ],
-        })
+        expect(getAllByText('Name is required').length).toBe(2)
       })
     })
   })

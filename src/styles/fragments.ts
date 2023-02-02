cbfbd221ec852dc9e0debbf85eae9f15c1ce5799
ToniMHeinonen <toni.heinonen@hotmail.fi>
2023-02-02 12:58:10 +0200

    Convert styles to TypeScript

--- src/styles/fragments.ts
+++ src/styles/fragments.ts
@@ -1,7 +1,30 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { css } from 'styled-components/native'
 import theme from '../components/theme'
 
-export const formFieldBaseStyles = css`
+interface PressedContentColorProps {
+  pressed?: boolean
+}
+
+interface FormFieldBaseStylesProps {
+  error?: boolean
+}
+
+interface CustomStyleProps {
+  // TODO: Make this work with custom css values without using any
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  style?: any
+}
+
+export const pressedContentColor = css<PressedContentColorProps>`
+  ${({ pressed }) =>
+    pressed &&
+    css`
+      background-color: ${theme.colors.contentBackground};
+    `}
+`
+
+export const formFieldBaseStyles = css<FormFieldBaseStylesProps>`
   background-color: ${theme.colors.appBackground};
   margin: 5px;
   padding: 5px;
@@ -25,14 +48,6 @@ export const formFieldBaseStyles = css`
   ${pressedContentColor}
 `
 
-export const pressedContentColor = css`
-  ${({ pressed }) =>
-    pressed &&
-    css`
-      background-color: ${theme.colors.contentBackground};
-    `}
-`
-
-export const customStyle = css`
+export const customStyle = css<CustomStyleProps>`
   ${({ style }) => style}
 `

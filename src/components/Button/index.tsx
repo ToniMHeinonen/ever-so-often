04a9e1cca20c465a11ba6bdc0ecda8c277d0d1e3
ToniMHeinonen <toni.heinonen@hotmail.fi>
2023-02-09 11:07:45 +0200

    Convert Button component to TypeScript

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,24 @@
+import { Pressable, View } from 'react-native'
+
+interface ButtonProps {
+  pressed: boolean
+}
+
+interface Props {
+  onPress: () => void
+  component?: React.FC<ButtonProps>
+  children: React.ReactNode[]
+}
+
+const Button = ({ onPress, component, ...props }: Props): JSX.Element => {
+  const Component = component ?? View
+  return (
+    <Pressable onPress={onPress} {...props}>
+      {({ pressed }): JSX.Element => (
+        <Component pressed={pressed}>{props.children}</Component>
+      )}
+    </Pressable>
+  )
+}
+
+export default Button
